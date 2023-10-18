9ee78ec0ad82ae781949d6cc2ac65b5a4ee26863
iqranaz240 <--unset>
2023-10-17 21:35:53 -0700

    memoization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { memo, useCallback, useMemo, useState } from 'react';
 
 function App() {
+  console.log("app peda huwa")
+  const [state1, setState1] = useState(0)
+  const [state2, setState2] = useState(0)
+  const [state3, setState3] = useState(32)
+
+  const obj = useMemo(() => {
+    return {
+      name: "sufiyan",
+      age: state3
+    }
+  }, [state3])
+
+  const addCounterHandler = useCallback(() => {
+    return function addCounterHandler() {
+      console.log("main chal gaya")
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {state1}
+      <button onClick={() => setState1(state1 + 1)}>
+        change state 1
+      </button>
+      <button onClick={() => setState2(state2 + 1)}>
+        change state 2
+      </button>
+      <button onClick={() => setState3(state3 + 1)}>
+        change state 3
+      </button>
+      <Comp1 prop1={state2} prop2={obj} addCounter={addCounterHandler} />
+    </>
   );
 }
 
 export default App;
+
+
+const Comp1 = memo(function Comp1({ prop1, prop2, addCounterHandler }) {
+  console.log("component 1 peda huwa")
+  console.log(prop2)
+  return (
+    <>
+      <h1>
+        {prop2.name} with age {prop2.age}
+      </h1>
+      <h1>
+        {prop1}
+      </h1>
+      <h1>Yeh hai mera Component 1</h1>
+    </>
+  )
+})
